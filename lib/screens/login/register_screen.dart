9f43fc745ed4ed1177d7e1a23c3e71ac87f26e8c
marh08192003 <riverita470@gmail.com>
2025-01-17 14:41:31 -0500

    Modificacion en la vista de registrar usuarios

--- lib/screens/login/register_screen.dart
+++ lib/screens/login/register_screen.dart
@@ -18,7 +18,8 @@ class _RegisterScreenState extends State<RegisterScreen> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
-  String? selectedUserType;
+  String? selectedUserType = "estudiante"; // Valor por defecto
+  bool isAdmin = false; // Se usará para verificar si el usuario es administrador
 
   final List<String> userTypes = [
     'estudiante',
@@ -28,89 +29,200 @@ class _RegisterScreenState extends State<RegisterScreen> {
   ];
 
   @override
+  void initState() {
+    super.initState();
+    _checkIfAdmin(); // Verificar si el usuario actual es administrador
+  }
+
+  void _checkIfAdmin() async {
+    final userInfo = await _authService.getUserInfo();
+    setState(() {
+      isAdmin = userInfo['userType']?.toLowerCase() == 'administrador';
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Register")),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      appBar: AppBar(
+        title: const Text("Crear cuenta"),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+        ),
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              TextField(
-                controller: idController,
-                decoration: const InputDecoration(labelText: "ID (Cédula)"),
-                keyboardType: TextInputType.number,
-              ),
-              TextField(
-                controller: firstNameController,
-                decoration: const InputDecoration(labelText: "First Name"),
-              ),
-              TextField(
-                controller: lastNameController,
-                decoration: const InputDecoration(labelText: "Last Name"),
-              ),
-              TextField(
-                controller: phoneController,
-                decoration: const InputDecoration(labelText: "Phone"),
-                keyboardType: TextInputType.phone,
-              ),
-              DropdownButtonFormField<String>(
-                value: selectedUserType,
-                items: userTypes.map((type) {
-                  return DropdownMenuItem<String>(
-                    value: type,
-                    child: Text(type),
-                  );
-                }).toList(),
-                onChanged: (value) {
-                  setState(() {
-                    selectedUserType = value;
-                  });
-                },
-                decoration: const InputDecoration(labelText: "User Type"),
-              ),
-              TextField(
-                controller: emailController,
-                decoration:
-                    const InputDecoration(labelText: "Institutional Email"),
-                keyboardType: TextInputType.emailAddress,
-              ),
-              TextField(
-                controller: passwordController,
-                decoration: const InputDecoration(labelText: "Password"),
-                obscureText: true,
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () async {
-                  final user = {
-                    "id": int.tryParse(idController.text),
-                    "firstName": firstNameController.text,
-                    "lastName": lastNameController.text,
-                    "phone": phoneController.text,
-                    "userType": selectedUserType,
-                    "institutionalEmail": emailController.text,
-                    "password": passwordController.text,
-                  };
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 20),
+                TextField(
+                  controller: idController,
+                  decoration: InputDecoration(
+                    labelText: "ID (Código institucional)",
+                    labelStyle: TextStyle(color: Colors.grey[700]),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+                const SizedBox(height: 20), // Espaciado uniforme
+                TextField(
+                  controller: firstNameController,
+                  decoration: InputDecoration(
+                    labelText: "Nombre",
+                    labelStyle: TextStyle(color: Colors.grey[700]),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20), // Espaciado uniforme
+                TextField(
+                  controller: lastNameController,
+                  decoration: InputDecoration(
+                    labelText: "Apellido",
+                    labelStyle: TextStyle(color: Colors.grey[700]),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20), // Espaciado uniforme
+                TextField(
+                  controller: phoneController,
+                  decoration: InputDecoration(
+                    labelText: "Teléfono",
+                    labelStyle: TextStyle(color: Colors.grey[700]),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                  keyboardType: TextInputType.phone,
+                ),
+                if (isAdmin) ...[
+                  const SizedBox(height: 20), // Espaciado uniforme
+                  DropdownButtonFormField<String>(
+                    value: selectedUserType,
+                    items: userTypes.map((type) {
+                      return DropdownMenuItem<String>(
+                        value: type,
+                        child: Text(type),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        selectedUserType = value;
+                      });
+                    },
+                    decoration: InputDecoration(
+                      labelText: "Cargo",
+                      labelStyle: TextStyle(color: Colors.grey[700]),
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+                ],
+                const SizedBox(height: 20), // Espaciado uniforme
+                TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    labelText: "Correo institucional",
+                    labelStyle: TextStyle(color: Colors.grey[700]),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                const SizedBox(height: 20), // Espaciado uniforme
+                TextField(
+                  controller: passwordController,
+                  decoration: InputDecoration(
+                    labelText: "Contraseña",
+                    labelStyle: TextStyle(color: Colors.grey[700]),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                  obscureText: true,
+                ),
+                const SizedBox(height: 30),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      final user = {
+                        "id": int.tryParse(idController.text),
+                        "firstName": firstNameController.text,
+                        "lastName": lastNameController.text,
+                        "phone": phoneController.text,
+                        "userType": isAdmin
+                            ? selectedUserType
+                            : "estudiante", // Establecer valor por defecto si no es admin
+                        "institutionalEmail": emailController.text,
+                        "password": passwordController.text,
+                      };
 
-                  final success = await _authService.register(user);
+                      final success = await _authService.register(user);
 
-                  if (success) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                          content: Text("User registered successfully!")),
-                    );
-                    Navigator.pop(context);
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                          content: Text("User registration failed!")),
-                    );
-                  }
-                },
-                child: const Text("Register"),
-              ),
-            ],
+                      if (success) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                              content: Text("Usuario registrado exitosamente!")),
+                        );
+                        Navigator.pop(context);
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                              content: Text("Error al registrar el usuario!")),
+                        );
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      minimumSize: const Size(double.infinity, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: const Text(
+                      "Registrarse",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+              ],
+            ),
           ),
         ),
       ),
